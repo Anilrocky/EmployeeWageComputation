1ed8639a9a1ed04bf6ab7e73878f0f6eb9daa020
Anilrocky <anilkumarac96@gmail.com>
2023-03-14 22:09:12 +0530

    [Anil] Add:Calculate employee wage for multiple companies

--- EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -3,7 +3,7 @@ namespace EmployeeWageComputation
 {
     public class EmployeeWage
     {
-        const int IS_PRESENT=1,WAGE_PER_HR=20,FULL_DAY_HR=8,PART_TIME_HR=4,IS_FULL_TIME=0,IS_PART_TIME=1,TOTAL_WORKING_DAYS=20,TOTAL_WORKING_HOURS=100;
+        const int IS_PRESENT = 1, FULL_DAY_HR = 8, PART_TIME_HR = 4, IS_FULL_TIME = 0, IS_PART_TIME = 1;
         public void Attendance()
         {
             Random random = new Random();
@@ -13,12 +13,12 @@ namespace EmployeeWageComputation
             else
                 Console.WriteLine("Employee is Absent");
         }
-        public void DailyEmpWage()
+        public void DailyEmpWage(string Company_Name,int Wage_Per_Hr,int Total_Working_Days,int Total_Working_Hrs)
         {
             int empHrs = 0;
             Random random = new Random();
             int check = random.Next(0, 3);
-            for (int i = 0; i <= TOTAL_WORKING_DAYS && empHrs <= TOTAL_WORKING_HOURS; i++)
+            for (int i = 0; i <= Total_Working_Days && empHrs <= Total_Working_Hrs; i++)
             {
                 switch (check)
                 {
@@ -33,7 +33,7 @@ namespace EmployeeWageComputation
                         break;
                 }
             }
-            int wage = WAGE_PER_HR * empHrs;
+            int wage = Wage_Per_Hr * empHrs;
             Console.WriteLine("Employee Wage is: " + wage);
         }
     }
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,7 +8,10 @@ namespace EmployeeWageComputation
             Console.WriteLine("Welcome to Employee Wage Computation");
             EmployeeWage employeeWage = new EmployeeWage();
             employeeWage.Attendance();
-            employeeWage.DailyEmpWage();
+            employeeWage.DailyEmpWage("Infosys", 35, 20, 100);
+            employeeWage.DailyEmpWage("CGI", 20, 25, 90);
+            employeeWage.DailyEmpWage("Spatial", 30, 24, 120);
+            employeeWage.DailyEmpWage("L&T", 20, 20, 130);
         }
     }
 }
